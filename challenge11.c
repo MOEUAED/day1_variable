166003c48578bf709bc21d6db6564e5f64e6a6e2
mouad ziyani <ziyanimouad@gmail.com>
2025-09-02 17:05:42 +0200

    challenge11

---
+++ challenge11.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+int main (){
+
+float longeur, largeur ,surface;
+
+ printf("entrer la longeur de rectangle : ");
+
+    scanf("%f",&longeur);
+
+     printf("entrer la largeur de rectangle : ");
+
+    scanf("%f",&largeur);
+
+
+surface = longeur * largeur ;
+
+printf("surface de rectangle  est : %f ", surface);
+return 0 ;
+
+}
