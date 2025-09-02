f35e07ad9e144c155d8e6fd0d28a1ea4b8469241
mouad ziyani <ziyanimouad@gmail.com>
2025-09-02 15:21:55 +0200

    challenge3

---
+++ challenge3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+float distance , yard ;
+
+printf("Entrer la distance en kilometre :");
+
+scanf("%f",&distance);
+
+yard = distance * 1093.61;
+printf("your distance en yard est : %.2f  yard\n", yard);
+
+
+return 0;
+}
